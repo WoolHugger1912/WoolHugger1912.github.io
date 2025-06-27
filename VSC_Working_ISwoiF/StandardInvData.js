59ccf0a82b3bf4c4c728f2130053885a80418383
WoolHugger1912 <gus.andelman@gmail.com>
2025-06-27 06:23:05 -0700

    Add files via upload

---
+++ VSC_Working_ISwoiF/StandardInvData.js
@@ -0,0 +1,141 @@
+document.getElementById("aceAcid").innerHTML += Math.max(0, localStorage.getItem("Acetic Acid"));
+document.getElementById("albaSPlus").innerHTML += Math.max(0, localStorage.getItem("Albaflow SF Plus"));
+document.getElementById("albaSConc").innerHTML += Math.max(0, localStorage.getItem("Albafluid SL Conc"));
+document.getElementById("albeA").innerHTML += Math.max(0, localStorage.getItem("Albegal A"));
+document.getElementById("albeE3B").innerHTML += Math.max(0, localStorage.getItem("Albegal E3 B"));
+document.getElementById("albeSet").innerHTML += Math.max(0, localStorage.getItem("Albegal Set"));
+document.getElementById("clariBac").innerHTML += Math.max(0, localStorage.getItem("Clarite Bac"));
+document.getElementById("colCle").innerHTML += Math.max(0, localStorage.getItem("Color Clear"));
+document.getElementById("eriE3S").innerHTML += Math.max(0, localStorage.getItem("Eriopon E3 Save"));
+document.getElementById("eriLan").innerHTML += Math.max(0, localStorage.getItem("Eriopon Lan"));
+document.getElementById("formAcid").innerHTML += Math.max(0, localStorage.getItem("Formic Acid"));
+document.getElementById("hydroPero").innerHTML += Math.max(0, localStorage.getItem("Hydrogen Peroxide"));
+document.getElementById("hydroSulf").innerHTML += Math.max(0, localStorage.getItem("Hydroxylamine Sulfate"));
+document.getElementById("ka2nLS").innerHTML += Math.max(0, localStorage.getItem("Kierolan A2N"));
+document.getElementById("knulLS").innerHTML += Math.max(0, localStorage.getItem("Kieralon NF. US Liq"));
+document.getElementById("miraLTD").innerHTML += Math.max(0, localStorage.getItem("Miralan LTD"));
+document.getElementById("miraTOP").innerHTML += Math.max(0, localStorage.getItem("Miralan TOP"));
+document.getElementById("saraPaw").innerHTML += Math.max(0, localStorage.getItem("Sarabid Paw"));
+document.getElementById("sodAsh").innerHTML += Math.max(0, localStorage.getItem("Soda Ash"));
+document.getElementById("sodAce").innerHTML += Math.max(0, localStorage.getItem("Sodium Acetate"));
+document.getElementById("sodBi").innerHTML += Math.max(0, localStorage.getItem("Sodium Bicarbonate"));
+document.getElementById("sodMeta").innerHTML += Math.max(0, localStorage.getItem("Sodium Metabisulfate"));
+document.getElementById("sodSulf").innerHTML += Math.max(0, localStorage.getItem("Sodium Sulfate"));
+document.getElementById("soloDE3").innerHTML += Math.max(0, localStorage.getItem("Solopol DE3"));
+document.getElementById("thiDio").innerHTML += Math.max(0, localStorage.getItem("Thiourea Dioxide"));
+document.getElementById("tnLS").innerHTML += Math.max(0, localStorage.getItem("Tinegal NT"));
+document.getElementById("ucnLS").innerHTML += Math.max(0, localStorage.getItem("Ultravon CN"));
+document.getElementById("uvddplLS").innerHTML += Math.max(0, localStorage.getItem("Univadine DPL"));
+document.getElementById("uvddfmLS").innerHTML += Math.max(0, localStorage.getItem("Univadine DFM"));
+document.getElementById("ufpLS").innerHTML += Math.max(0, localStorage.getItem("UV Fast PLF"));
+document.getElementById("ufwlLS").innerHTML += Math.max(0, localStorage.getItem("UV Fast W. Liq."));
+
+document.getElementById("bxyLS").innerHTML += Math.max(0, localStorage.getItem("Bemaplex Yellow M-T"));
+document.getElementById("bvy4gLS").innerHTML += Math.max(0, localStorage.getItem("Bezaktiv Yellow WO-4G"));
+document.getElementById("bvy4rLS").innerHTML += Math.max(0, localStorage.getItem("Bezaktiv Yellow WO-4R"));
+document.getElementById("bdyLS").innerHTML += Math.max(0, localStorage.getItem("Bemacid Yellow E-4G"));
+document.getElementById("doraYelo").innerHTML += Math.max(0, localStorage.getItem("Doracryl Golden Yellow"));
+document.getElementById("lana2RYelo").innerHTML += Math.max(0, localStorage.getItem("Lanaset Yellow 2R"));
+document.getElementById("lana4GNYelo").innerHTML += Math.max(0, localStorage.getItem("Lanaset Yellow 4GN"));
+document.getElementById("lanaCEYelo").innerHTML += Math.max(0, localStorage.getItem("Lanasol Golden Yellow CE"));
+document.getElementById("lana4GYelo").innerHTML += Math.max(0, localStorage.getItem("Lanasol Yellow 4G"));
+document.getElementById("tect2GYelo").innerHTML += Math.max(0, localStorage.getItem("Tectilon Yellow 2G"));
+document.getElementById("tect3RYelo").innerHTML += Math.max(0, localStorage.getItem("Tectilon Yellow 3R"));
+document.getElementById("telo4RYelo").innerHTML += Math.max(0, localStorage.getItem("Telon Yellow 4R"));
+document.getElementById("teraCRYelo").innerHTML += Math.max(0, localStorage.getItem("Terasil Yellow CR"));
+document.getElementById("teraBRLFYelo").innerHTML += Math.max(0, localStorage.getItem("Teratop Yellow BRLF"));
+document.getElementById("teraGWLYelo").innerHTML += Math.max(0, localStorage.getItem("Tera(top/sil) Yellow GWL"));
+document.getElementById("lanaRNOrng").innerHTML += Math.max(0, localStorage.getItem("Lanaset Orange RN"));
+document.getElementById("teloMGSNOrng").innerHTML += Math.max(0, localStorage.getItem("Telon Orange M-GSN"));
+document.getElementById("teraTCOrng").innerHTML += Math.max(0, localStorage.getItem("Terasil Orange TC"));
+document.getElementById("tera4RLOrng").innerHTML += Math.max(0, localStorage.getItem("Terasil Orange 4RL"));
+document.getElementById("bezRed").innerHTML += Math.max(0, localStorage.getItem("Bezaktiv Red WO-BB"));
+document.getElementById("bemaMTRed").innerHTML += Math.max(0, localStorage.getItem("Bemaplex Red M-T"));
+document.getElementById("bemaF4BRed").innerHTML += Math.max(0, localStorage.getItem("Bemacid Red F-4B"));
+document.getElementById("dorac2BRed").innerHTML += Math.max(0, localStorage.getItem("Dorasyn Red C-2B"));
+document.getElementById("eriRed").innerHTML += Math.max(0, localStorage.getItem("Erionyl Red B-10B"));
+document.getElementById("lana2BRed").innerHTML += Math.max(0, localStorage.getItem("Lanaset Red 2B"));
+document.getElementById("lanaBBord").innerHTML += Math.max(0, localStorage.getItem("Lanaset Bordeaux B"));
+document.getElementById("lanaCEBord").innerHTML += Math.max(0, localStorage.getItem("Lanasol Bordeaux CE"));
+document.getElementById("lana6GRed").innerHTML += Math.max(0, localStorage.getItem("Lanasol Red 6G"));
+document.getElementById("lanaCERed").innerHTML += Math.max(0, localStorage.getItem("Lanasol Red CE"));
+document.getElementById("lanaGRed").innerHTML += Math.max(0, localStorage.getItem("Lanaset Red G"));
+document.getElementById("lanaF3GLScar").innerHTML += Math.max(0, localStorage.getItem("Lanasyn Scarlet F-3GL"));
+document.getElementById("maxiRed").innerHTML += Math.max(0, localStorage.getItem("Maxilon Red GRL"));
+document.getElementById("optiScar").innerHTML += Math.max(0, localStorage.getItem("Optilan Scarlet MF-GL"));
+document.getElementById("tect2BRed").innerHTML += Math.max(0, localStorage.getItem("Tectilon Red 2B"));
+document.getElementById("teloRed").innerHTML += Math.max(0, localStorage.getItem("Telon Red A-FG"));
+document.getElementById("teloRhod").innerHTML += Math.max(0, localStorage.getItem("Telon Rhodamine"));
+document.getElementById("tera3BLRed").innerHTML += Math.max(0, localStorage.getItem("Terasil Red 3BL"));
+document.getElementById("teraCBRed").innerHTML += Math.max(0, localStorage.getItem("Terasil Red CB"));
+document.getElementById("teraRRed").innerHTML += Math.max(0, localStorage.getItem("Terasil Red R"));
+document.getElementById("teraPink").innerHTML += Math.max(0, localStorage.getItem("Teratop Pink 2GLA"));
+document.getElementById("lanaViol").innerHTML += Math.max(0, localStorage.getItem("Lanaset Violet B"));
+document.getElementById("teloViol").innerHTML += Math.max(0, localStorage.getItem("Telon Violet M-RWN"));
+document.getElementById("lanaGrey").innerHTML += Math.max(0, localStorage.getItem("Lanaset Grey G"));
+document.getElementById("doraBlue").innerHTML += Math.max(0, localStorage.getItem("Doracid Blue SBL"));
+document.getElementById("bezBlue").innerHTML += Math.max(0, localStorage.getItem("Bezaktiv Blue WO-DB"));
+document.getElementById("lana2RABlue").innerHTML += Math.max(0, localStorage.getItem("Lanaset Blue 2RA"));
+document.getElementById("lana5GBlue").innerHTML += Math.max(0, localStorage.getItem("Lanaset Blue 5G"));
+document.getElementById("lana3GBlue").innerHTML += Math.max(0, localStorage.getItem("Lanasol Blue 3G"));
+document.getElementById("lana3RBlue").innerHTML += Math.max(0, localStorage.getItem("Lanasol Blue 3R"));
+document.getElementById("lana8GBlue").innerHTML += Math.max(0, localStorage.getItem("Lanasol Blue 8G"));
+document.getElementById("lanaCEBlue").innerHTML += Math.max(0, localStorage.getItem("Lanasol Blue CE"));
+document.getElementById("maxiBlue").innerHTML += Math.max(0, localStorage.getItem("Maxilon Blue SL"));
+document.getElementById("tect4RSBlue").innerHTML += Math.max(0, localStorage.getItem("Tectilon Blue 4RS"));
+document.getElementById("tect6GBlue").innerHTML += Math.max(0, localStorage.getItem("Tectilon Blue 6G"));
+document.getElementById("teloMGLWBlue").innerHTML += Math.max(0, localStorage.getItem("Telon Blue M-GLW"));
+document.getElementById("teloRRBlue").innerHTML += Math.max(0, localStorage.getItem("Telon Blue RR"));
+document.getElementById("tera3RLBlue").innerHTML += Math.max(0, localStorage.getItem("Terasil Blue 3RL"));
+document.getElementById("teraGLFBlue").innerHTML += Math.max(0, localStorage.getItem("Tera(sil/top) Blue GLF"));
+document.getElementById("teraRBSBlue").innerHTML += Math.max(0, localStorage.getItem("Terasil Blue RBS"));
+document.getElementById("teraBGEBlue").innerHTML += Math.max(0, localStorage.getItem("Teratop Blue BGE"));
+document.getElementById("teraBLFBlue").innerHTML += Math.max(0, localStorage.getItem("Teratop Blue BLF"));
+document.getElementById("bemaNavy").innerHTML += Math.max(0, localStorage.getItem("Bemaplex Navy M-T"));
+document.getElementById("lanaCFRNavy").innerHTML += Math.max(0, localStorage.getItem("Lanasyn Navy CF-R"));
+document.getElementById("lanaCENavy").innerHTML += Math.max(0, localStorage.getItem("Lanasol Navy CE"));
+document.getElementById("lanaGreen").innerHTML += Math.max(0, localStorage.getItem("Lanaset Green B"));
+document.getElementById("bemaBrown").innerHTML += Math.max(0, localStorage.getItem("Bemaplex Brown D-BD"));
+document.getElementById("lanaBNABrown").innerHTML += Math.max(0, localStorage.getItem("Lanaset Brown B-NA"));
+document.getElementById("lanaGBrown").innerHTML += Math.max(0, localStorage.getItem("Lanaset Brown G"));
+document.getElementById("bezBlck").innerHTML += Math.max(0, localStorage.getItem("Bezaktiv Black WO-TF"));
+document.getElementById("isolBlck").innerHTML += Math.max(0, localStorage.getItem("Isolan Black 2S-LD"));
+document.getElementById("lanaCEBlck").innerHTML += Math.max(0, localStorage.getItem("Lanasol Black CE"));
+document.getElementById("lanaCEDpBlck").innerHTML += Math.max(0, localStorage.getItem("Lanasol Deep Black CE-R"));
+
+function exportTableToExcel(tableId) {
+    // Get the table element using the provided ID
+    const table = document.getElementById(tableId);
+  
+    // Extract the HTML content of the table
+    const html = table.outerHTML;
+  
+    // Create a Blob containing the HTML data with Excel MIME type
+    const blob = new Blob([html], {type: 'application/vnd.ms-excel'});
+  
+    // Create a URL for the Blob
+    const url = URL.createObjectURL(blob);
+  
+    // Create a temporary anchor element for downloading
+    const a = document.createElement('a');
+    a.href = url;
+  
+    // Set the desired filename for the downloaded file
+    a.download = 'table.xls';
+  
+    // Simulate a click on the anchor to trigger download
+    a.click();
+  
+    // Release the URL object to free up resources
+    URL.revokeObjectURL(url);
+
+  }
+  
+  // Attach the function to the export button's click event
+  document.getElementById('exportButton').addEventListener('click', function() {
+    exportTableToExcel('tableId');
+    
+    if (window.location.href === mathInven.html) {
+        window.location.href = MasterInv.html;
+    };
+  });
